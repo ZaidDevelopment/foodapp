1562c8316795edc0ef05cce805cb1e0ab22d39d7
Zaid <zaid.a@hotmail.com>
2024-07-12 19:29:20 +0100

    Remove padding-right from col

--- src/components/RecipeServings.jsx
+++ src/components/RecipeServings.jsx
@@ -1,19 +1,19 @@
 export default function RecipeServings({ food }) {
   return (
     <div className="row" style={{ opacity: "70%" }}>
-      <p className="col-6 mb-0">
+      <p className="col-6 mb-0 pe-0">
         <strong>Time: </strong>
         {food.readyInMinutes} Minutes
       </p>
-      <p className="col-6 mb-0">
+      <p className="col-6 mb-0 pe-0">
         <strong>Serves: </strong>
         {food.servings}
       </p>
-      <p className="col-6">
+      <p className="col-6 pe-0">
         <strong>Diet: </strong>
         {food.vegeterian ? "Vegetarian" : "Non-Vegeterian"}
       </p>
-      <p className="col-6">
+      <p className="col-6 pe-0">
         <strong>$: </strong>
         {(food.pricePerServing / 100).toFixed(2)} Per serving
       </p>
